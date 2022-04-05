7dff9609a2378d978ee77ead5919128db7a42338
bhagwat-99 <shindebhagavat030@gmail.com>
2022-04-05 20:01:38 +0530

    first commit

---
+++ math_custom.c
@@ -0,0 +1,32 @@
+#include math_custom.h
+#include <stdio.h>
+
+float add(float num1, float num2)
+{
+        float result;
+        result = num1 + num2;
+        return result;
+}
+
+float substract(float num1, float num2)
+{
+        float result;
+        if()
+        {}
+        result = num1 + num2;
+        return result;
+}
+
+float multiply(float num1, float num2)
+{
+        float result;
+        result = num1 + num2;
+        return result;
+}
+
+float divide(float num1, float num2)
+{
+        float result;
+        result = num1 + num2;
+        return result;
+}
---
+++ math_custom.h
@@ -0,0 +1,14 @@
+#ifdef MATH_CUSTOM.h
+#define MATH_CUSTOM.h
+
+float add(float num1, float num2);
+
+float substract(float num1, float num2);
+
+float multiply(float num1, float num2);
+
+
+float divide(float num1, float num2);
+
+
+#endif
