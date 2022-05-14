1c5c544d3e9c0f9eb986b3033ed96f9769ddae94
bhagwat-99 <shindebhagavat030@gmail.com>
2022-05-14 09:04:59 +0530

    sleep change to 1 min

--- file/main.c
+++ file/main.c
@@ -101,7 +101,7 @@ int main()
                         return -1;
                 }
                
-                sleep(10);
+                sleep(60);
         }        
         return 0;
 }
